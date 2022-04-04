1ac78f7c23c0ae73fc99ad80a420e07cd0758c27
kmiller <kmmiller418@gmail.com>
2022-04-03 21:12:09 -0400

    Add scoreboard, add computer hand

--- script.js
+++ script.js
@@ -9,50 +9,85 @@ const winConditions = {
 getHand = (array) => {
   let index = parseInt(Math.random() * 10) % 3;
   return array[index];
-}
-
-let p1 = {
-    name: "",
-    hand: "",
-    score: 0
 };
 
-let cpu = {
-    name: "computer",
-    hand: getHand,
-    score: 0
+const p1 = {
+  name: "",
+  hand: "",
+  score: 0,
+};
+
+const cpu = {
+  name: "computer",
+  hand: getHand,
+  score: 0,
 };
 
 //queryselectors
 const submit = document.querySelector(".user-submit");
 const handButton = document.querySelector("div.container");
-
+const userName = document.querySelector(".user-name");
+const userScore = document.querySelector(".user-score");
+const computerScore = document.querySelector(".computer-score");
+const computerHand = document.querySelector(".computer-hand");
 
 //event listeners
 submit.addEventListener("click", () => {
   p1.name = document.querySelector("input").value;
   console.log(p1.name);
+  userName.innerHTML = p1.name;
+  userName.style.color = 'navy';
 });
 
 handButton.addEventListener("click", (e) => {
-    p1.hand = e.target.parentNode.value;
-    playRound();
-})
-
+  p1.hand = e.target.parentNode.value;
+  playRound();
+});
 
 //game logic
 
 playRound = () => {
   cpu.hand = getHand(hands);
+  showCPUHand(cpu.hand);
 
-  if (p1.hand === cpu.hand) {
-      alert('issa tie')
-      return null;
-} else if (winConditions[p1.hand] === cpu.hand) {
-    alert('you win');
-    return p1;
-} else {
-    alert('computah win');
-    return cpu;
+  setTimeout(function () {
+    if (p1.hand === cpu.hand) {
+      alert("issa tie");
+    } else if (winConditions[p1.hand] === cpu.hand) {
+      alert("you win");
+      p1.score++;
+    } else {
+      alert("computah win");
+      cpu.score++;
+    }
+
+    updateScore();
+
+  }, 1000);
+
+};
+
+updateScore = () => {
+  userScore.innerHTML = p1.score;
+  computerScore.innerHTML = cpu.score;
 }
+
+showCPUHand = (hand) => {
+  const icons = [
+    "<i class='bi bi-gem'></i>",
+    "<i class='bi bi-sticky'></i>",
+    "<i class='bi bi-scissors'></i>",
+  ];
+
+  switch (hand) {
+    case "rock":
+      computerHand.innerHTML = icons[0];
+      break;
+    case "paper":
+      computerHand.innerHTML = icons[1];
+      break;
+    case "scissors":
+      computerHand.innerHTML = icons[2];
+      break;
+  }
 };
